1ad4527fe5fc8f42e63f0e3aa88966fd3622902c
Bazv <brandonv.zamora@gmail.com>
2025-01-03 14:24:35 -0600

    fix:  Cambia archivos .ts a scripts inline

--- src/ts/infoAlert.ts
+++
@@ -1,8 +0,0 @@
-const alertElement = document.getElementById("info-alert");
-const closeButton = document.getElementById("close-alert");
-
-if (closeButton && alertElement) {
-    closeButton.addEventListener("click", () => {
-        alertElement.classList.add("hidden");
-    });
-}
